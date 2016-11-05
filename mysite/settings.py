5ab7f534e32095393b300ce768a5084147a5d856
mithawardhani <mithawardhani@users.noreply.github.com>
2016-11-05 14:53:58 -0700

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$jik=_2-$@59bul+xi$9q4_zlg@^47_4v4ndhmhz+7=okadsnm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
